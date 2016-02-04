b971b25299cac401dca0237a90d598bc2b45d901
Chris Cates <codeviolet@chriscates.ca>
2016-02-04 16:45:26 -0500

    Updated app

--- public/scripts/tablefill.js
+++ public/scripts/tablefill.js
@@ -1,86 +1,35 @@
 var app = angular.module("todolist", []);
 
-
-
-r.table('tweetlist').insert([
-    { name: "William Adama", tv_show: "Battlestar Galactica",
-      posts: [
-        {title: "Decommissioning speech", content: "The Cylon War is long over..."},
-        {title: "We are at war", content: "Moments ago, this ship received word..."},
-        {title: "The new Earth", content: "The discoveries of the past few days..."}
-      ]
-    },
-    { name: "Laura Roslin", tv_show: "Battlestar Galactica",
-      posts: [
-        {title: "The oath of office", content: "I, Laura Roslin, ..."},
-        {title: "They look like us", content: "The Cylons have the ability..."}
-      ]
-    },
-    { name: "Jean-Luc Picard", tv_show: "Star Trek TNG",
-      posts: [
-        {title: "Civil rights", content: "There are some words I've known since..."}
-      ]
-    }
-]).run(connection, function(err, result) {
-    if (err) throw err;
-    console.log(JSON.stringify(result, null, 2));
-})
-
-
-
-
-
-
-
 app.controller("controller", ["$scope", "$http", function($scope, $http){
   $scope.title = "My todo list";
   //$scope.smalllist = "Hello";
-  $scope.items = [
-    "Pork chops",
-    "muffins",
-    "beer"
-  ]
+  $scope.items = []
   $scope.twitterers = [];
 
   $http({
     "method": "GET",
-    "url": "./dataset/tabledata.json"
+    "url": "/job"
   }).then(function(res) {
-    //console.log("Here is the response data " + JSON.stringify(res.data));
-    $scope.tweetfields = res.data;
-    /*res.data.map(function(tweetfields) {
-    $scope.twitterers.push(tweetfields.created_at);
-    })*/
-    res.data.map(function(item) {
-      $scope.items.push(item.created_at);
-    })
+    $scope.items = res.data;
   })
 
   $scope.todoInput  = "";
   $scope.addTodo = function(){
-    $scope.items.push(
-      $scope.todoInput
-    );
-    $scope.todoInput = "";
+
+    $http({
+      "method": "POST",
+      "url": "/job",
+      "data": {
+        "company": $scope.inputCompany,
+        "tweet": $scope.inputTweet
+      }
+    }).then(function(res) {
+      $scope.items.push({
+        "company": $scope.inputCompany,
+        "tweet": $scope.inputTweet
+      });
+      $scope.inputCompany = "";
+      $scope.inputTweet = "";
+    })
   }
-
 }])
-
-/*$(document).ready(function(){
-  $.get("./dataset/tabledata.json", function(result) {
-    console.log(result);
-  });
-});*/
-
-
-
-/*
-console.log(window.jQuery);
-$.getJSON("package.json", function(json) {
-  console.log(json);
-});
-alert(window.jQuery);
-  $("#container").hide();
-  console.log(testdata.id_str);
-//  alert(testdata.stringify);
-*/
--- server.js
+++ server.js
@@ -1,24 +1,42 @@
-/*
-  Some prelim notes:
-  1) Port can be specified vai process.env, if you wanted to do this via bash, you do PORT=9001 node sample.js
-  2) The wildcard (*) notates all unused routes, this gives you the ability to server index.html on all unused routes, very handy for SPAs
-  3) express.static() enables you to server static content from a resource or directory. This is handy for css, and, front end javascript files.
-*/
-
 //Require dependencies
 var express = require('express');
 var app = express();
 var path = require('path');
 var r = require("rethinkdbdash")();
+var bodyParser = require("body-parser");
+
 require("rethink-config")({
 "r": r,
 "database": "Twitterjobbot",
 "tables":["tweetlist"]
 });
 
+//Parse JSON/Forms
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 //Server all content in the public folder.
 app.use(express.static('./public'));
 
+//My first POST request
+app.post("/job", function(req,res,next) {
+  r.db("Twitterjobbot").table("tweetlist").insert({
+    "company": req.body.company,
+    "tweet": req.body.tweet,
+    "date": new Date()
+  }).then(function() {
+    res.status(200).send("Job posted.")
+  })
+})
+
+//My first GET request
+app.get("/job", function(req,res,next) {
+  r.db("Twitterjobbot").table('tweetlist').then(function(result) {
+    res.status(200).send(result);
+  })
+})
 
 //Assume that there is a folder named public with index.html in it.
 app.get('*', function(request, result) {
